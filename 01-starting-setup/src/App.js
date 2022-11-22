78a95b7b2cbfe2d0c1dd04cd44bd26a53080c4b6
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-11-22 23:14:57 +0300

    start project

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <p>This is new!</p>
+    </div>
+  );
+}
+
+export default App;
