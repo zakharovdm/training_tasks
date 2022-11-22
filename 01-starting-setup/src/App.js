5469b28639f76883cd2323bc4540c7c61e938ed2
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-11-22 23:27:29 +0300

    add button for toggle paragraph

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHanlder = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph)
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <p>This is new!</p>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHanlder}>Toggle paragraph</Button>
     </div>
   );
 }
