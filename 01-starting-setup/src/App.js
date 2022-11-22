d6fd0f435baa3b477d8a9d4abe939638e73cc06f
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-11-22 23:44:25 +0300

    add demo output for examlpe

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 
 import './App.css';
 
@@ -13,7 +14,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph}/>
       <Button onClick={toggleParagraphHanlder}>Toggle paragraph</Button>
     </div>
   );
