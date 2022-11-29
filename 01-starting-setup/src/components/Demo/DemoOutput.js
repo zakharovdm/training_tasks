2e12c6e288dd7e5722787f7cd978261f615900c6
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-11-29 23:39:25 +0300

    feat: add react memo

--- 01-starting-setup/src/components/Demo/DemoOutput.js
+++ 01-starting-setup/src/components/Demo/DemoOutput.js
@@ -1,5 +1,7 @@
-const DemoOutput = (props) => {
-  return <p>{props.show ? "This is new!" : ""}</p>
-}
+import React from "react";
 
-export default DemoOutput;
+const DemoOutput = (props) => {
+  return <p>{props.show ? "This is new!" : ""}</p>;
+};
+
+export default React.memo(DemoOutput);
